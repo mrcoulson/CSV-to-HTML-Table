43c1f0aec43f01ce6e9ed0df9fd768ce5e3550dc
Jeremy Coulson <jeremycoulson@gmail.com>
2012-10-03 11:33:36 -0400

    An example of loading the HTML with an include.

---
+++ example.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="CSV with Python." />
+	<meta name="author" content="Jeremy's code." />
+	<title>Addresses</title>
+	<link rel="stylesheet" href="tablestyle.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+	<script src="addresshoverscript.js"></script>
+</head>
+
+<body>
+	
+	<?php include "table.html" ?>
+
+</body>
+</html>
